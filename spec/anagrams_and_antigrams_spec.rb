736f4c7ea6d7799e108723e80277766f0bfa1891
Jon Stump <jmstump@gmail.com>
2021-02-26 10:24:35 -0800

    good pass on test checking that different cases are okay

--- spec/anagrams_and_antigrams_spec.rb
+++ spec/anagrams_and_antigrams_spec.rb
@@ -14,4 +14,8 @@ describe('Anagrams#anagrams_and_antigrams') do
     word = Anagrams.new()
     expect(word.word_check('ruby bury')).to(eq("ruby and bury are anagrams"))
   end
+  it('Will still check if inputs are anagrams if they have different cases') do
+    word = Anagrams.new()
+    expect(word.word_check('Eat Tea')).to(eq("Eat and Tea are anagrams"))
+  end
 end
