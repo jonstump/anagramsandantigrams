b0841b113fb38b27d7cf8f45b32147f5e60dbf86
Jon Stump <jmstump@gmail.com>
2021-02-26 14:28:09 -0800

    will continue to show work towards passing test on phrases

--- lib/anagrams_and_antigrams.rb
+++ lib/anagrams_and_antigrams.rb
@@ -12,25 +12,26 @@ class Anagrams
   end
 
   def anagrams_and_antigrams(user_input)
-    lower_user_words_1 = user_input.downcase().split(' ')
-    lower_user_words_2 = user_input.downcase().split(' ')
+    @lower_user_words_1 = user_input.downcase().split(' ')
+    @lower_user_words_2 = user_input.downcase().split(' ')
     user_words = user_input.split(' ')
+    # binding pry
     i = 0
     until i >= user_words.length
       i = i + 1
-      lower_user_words_1.each do |word|
-      
-      lower_user_words_1.split('').each do |letter|
-        if lower_user_words.split('').include?(letter)
-          @result = "#{user_words[0]} and #{user_words[1]} are anagrams"
-        elsif unless lower_user_words[1].split('').include?(letter)
-          @result = "#{user_words[0]} and #{user_words[1]} are antigrams"
+      @lower_user_words_1.each do |word|
+        word.split('').each do |letter|
+          if @lower_user_words_2[i].split('').include?(letter)
+            @result = "#{user_words[i]} and #{user_words[i+1]} are anagrams"
+          elsif unless @lower_user_words_2[i].split('').include?(letter)
+            @result = "#{user_words[i]} and #{user_words[i+1]} are antigrams"
+            end
+          else  
+            @result = "#{user_words[i]} and #{user_words[i+1]} are not anagrams"
           end
-        else  
-          @result = "#{user_words[0]} and #{user_words[1]} are not anagrams"
-        end
+          end
       end
-    end
     @result
+    end
   end
 end
