586e3b91b01c636a3dda749044533da4c2143521
Jon Stump <jmstump@gmail.com>
2021-02-26 09:37:26 -0800

    good fail on test for anagram

--- lib/anagrams_and_antigrams.rb
+++ lib/anagrams_and_antigrams.rb
@@ -2,9 +2,17 @@ class Anagrams
   def word_check(user_input)
     vowels_array = %w(a e i o u y)
     user_input.split('').each do |letter|
-      unless vowels_array.include?(letter)
-        return result = "#{user_input} is not a word"        
+      if vowels_array.include?(letter)
+        anagrams_and_antigrams(user_input)
+      else
+        @result = "#{user_input} is not a word" 
       end
     end
+    @result
+  end
+
+  def anagrams_and_antigrams(user_input)
+    nil
   end
 end
+
--- spec/anagrams_and_antigrams_spec.rb
+++ spec/anagrams_and_antigrams_spec.rb
@@ -2,9 +2,16 @@ require('rspec')
 require('pry')
 require('anagrams_and_antigrams')
 
-describe('anagrams_and_antigrams') do
+describe('Anagrams#word_check') do
   it('Will check if inputs are words') do
     word = Anagrams.new()
     expect(word.word_check('trk')).to(eq("trk is not a word"))
+  end
+end
+
+describe('Anagrams#anagrams_and_antigrams') do
+  it('Will check if inputs are anagrams') do
+    word = Anagrams.new()
+    expect(word.word_check('ruby bury')).to(eq("ruby and bury are anagrams"))
   end
 end
