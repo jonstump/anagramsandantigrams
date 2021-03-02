4283292a506cb0e6d94db91ef07139c2f68d851a
Jon Stump <jmstump@gmail.com>
2021-03-01 21:19:13 -0800

    adds older code for additional testing

--- lib/anagrams_and_antigrams.rb
+++ lib/anagrams_and_antigrams.rb
@@ -1,85 +1,59 @@
-class Anagrams
-  def initialize
-    @result_array = []
-  end
+# class Anagrams
+#   def initialize
+#     @result_array = []
+#   end
   
+#   def word_check(user_input)
+#     # @result_array = []
+#     vowels_array = %w(a e i o u y)
+#     word_array = user_input.split('')
+#     loop do
+#       if word_array.include?(vowels_array)
+#         @result_array << "#{user_input} is not a word" 
+#       else anagrams_and_antigrams(user_input)
+#       end
+#       break
+#       end
+#       @result_array
+#     end
+
+#   def anagrams_and_antigrams(user_input)
+#     user_words = user_input.split(' ')
+#     user_words.each do |word|
+#       if word.chars.sort == word.chars.sort
+#         @result_array << "#{word[0]} and #{word[1]} are anagrams"
+#         end
+#       end
+#     end
+#   end
+
+class Anagrams
   def word_check(user_input)
-    # @result_array = []
     vowels_array = %w(a e i o u y)
     user_input.split('').each do |letter|
       if vowels_array.include?(letter)
         anagrams_and_antigrams(user_input)
+      else
+        @result = "#{user_input} is not a word" 
       end
     end
-    @result_array << "#{user_input} is not a word" 
-    @result_array.join(', ')
+    @result
   end
 
   def anagrams_and_antigrams(user_input)
+    lower_user_words = user_input.downcase().split(' ')
     user_words = user_input.split(' ')
-    user_words.each do |word|
-      if word.chars.sort == word.chars.sort && word != word
-        @result_array << "#{word} and #{word} are anagrams"
+    lower_user_words[0].split('').each do |letter|
+      if lower_user_words[1].split('').include?(letter)
+        @result = "#{user_words[0]} and #{user_words[1]} are anagrams"
+      elsif unless lower_user_words[1].split('').include?(letter)
+        @result = "#{user_words[0]} and #{user_words[1]} are antigrams"
         end
+      else  
+        @result = "#{user_words[0]} and #{user_words[1]} are not anagrams"
       end
     end
-
-
-
-
-
-
-  # def anagrams_and_antigrams(user_input)
-  #   @lower_user_words_1 = user_input.downcase().split(' ')
-  #   @result_array = []
-  #   user_words = user_input.split(' ')
-  #   i = 0
-  #   until i >= user_words.length
-  #     @lower_user_words_1.each do |word|
-  #       if word != word
-  #         word.split('').each do |letter|
-  #           if words.split('').includes?(letter)
-  #             @result_array << "#{user_words[i]} and #{user_words[(i + 1)]} are anagrams"
-  #           elsif unless (word.split('').include?(letter)
-  #             @result_array << "#{user_words[i]} and #{user_words[(i + 1)]} are antigrams"
-  #             end
-  #           else
-  #             @result_array << "#{user_words[i]} and #{user_words[i+1]} are not anagrams"
-  #         end
-  #       end
-  #     end
-  #     i = i + 1
-  #     end
-  #   @result_array
-  #   end
-  # end
-
-
-
-# def anagrams_and_antigrams(user_input)
-#   @lower_user_words_1 = user_input.downcase().split(' ')
-#   @result_array = []
-#   user_words = user_input.split(' ')
-#   i = 0
-#   until i >= user_words.length
-#     @lower_user_words_1.each do |word|
-#         word.split('').each do |letter|
-#           if word.split('').include?(letter) && word != word
-#             @result_array << "#{user_words[i]} and #{user_words[(i + 1)]} are anagrams"
-#           elsif unless (word.split('').include?(letter) && word != word)
-#             @result_array << "#{user_words[i]} and #{user_words[(i + 1)]} are antigrams"
-#           end
-#         else  
-#           @result_array << "#{user_words[i]} and #{user_words[i+1]} are not anagrams"
-#         end
-#       end
-#     end
-#     i = i + 1
-#   end
-#   end
-# @result_array
-
-
-
-
+    @result
+  end
 end
+
