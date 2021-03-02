7c97fbeb2d625d77c427d1e7bbdbdf8c1eaf634d
Jon Stump <jmstump@gmail.com>
2021-03-01 22:08:09 -0800

    add weird test results that show issues with branching logic

--- lib/anagrams_and_antigrams.rb
+++ lib/anagrams_and_antigrams.rb
@@ -28,29 +28,32 @@
 #   end
 
 class Anagrams
+  def initialize
+    @result = []
+  end
+
   def word_check(user_input)
-    vowels_array = %w(a e i o u y)
-    user_input.split('').each do |letter|
-      if vowels_array.include?(letter)
+      if user_input.include?("a" || "e" || "i" || "o" || "u" || "y")
         anagrams_and_antigrams(user_input)
       else
         @result = "#{user_input} is not a word" 
       end
     end
     @result
-  end
+
+
 
   def anagrams_and_antigrams(user_input)
     lower_user_words = user_input.downcase().split(' ')
     user_words = user_input.split(' ')
     lower_user_words[0].split('').each do |letter|
       if lower_user_words[1].split('').include?(letter)
-        @result = "#{user_words[0]} and #{user_words[1]} are anagrams"
+        @result << "#{user_words[0]} and #{user_words[1]} are anagrams"
       elsif unless lower_user_words[1].split('').include?(letter)
-        @result = "#{user_words[0]} and #{user_words[1]} are antigrams"
+        @result << "#{user_words[0]} and #{user_words[1]} are antigrams"
         end
       else  
-        @result = "#{user_words[0]} and #{user_words[1]} are not anagrams"
+        @result << "#{user_words[0]} and #{user_words[1]} are not anagrams"
       end
     end
     @result
