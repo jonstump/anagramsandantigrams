9ae3ec491064adb39b9f75e4fd658a7bc78ce602
Jon Stump <jmstump@gmail.com>
2021-03-04 17:13:26 -0800

    refactor code to pass test for word checker

--- lib/anagrams_and_antigrams.rb
+++ lib/anagrams_and_antigrams.rb
@@ -1,28 +1,19 @@
 class Anagrams
-  def initalize
-    @result = []
-  end
   
   def word_check(user_input1, user_input2)
     vowels_array = %w(a e i o u y)
-    user_input1.split(' ').each do |word|
+    checker = user_input1 + user_input2
+    checker.split(' ').each do |word|
       if word.chars.include?(vowels_array)
-        true
+        anagrams_and_antigrams(user_input1, user_input2)
       else
-        @result = "#{user_input1} is not a word. " 
+        @result = "#{user_input1} and #{user_input2} are not, or do not include, words." 
       end
-      user_input2.split(' ').each do |word|
-        if word.chars.include?(vowels_array)
-          true
-        else
-          @result << "#{user_input2} is not a word" 
-        end
-    end
     end
     @result
   end
 
-  def anagrams_and_antigrams(user_input)
+  def anagrams_and_antigrams(user_input1, user_input2)
     lower_user_words = user_input.downcase().split(' ')
     user_words = user_input.split(' ')
     lower_user_words[0].split('').each do |letter|
