f480ea669683f3d11b8f6e3a241b49147b85907a
Jon Stump <jmstump@gmail.com>
2021-03-04 17:33:06 -0800

    fail on case test

--- spec/anagrams_and_antigrams_spec.rb
+++ spec/anagrams_and_antigrams_spec.rb
@@ -14,10 +14,10 @@ describe('Anagrams#anagrams_and_antigrams') do
     word = Anagrams.new()
     expect(word.word_check('ruby', 'bury')).to(eq("ruby and bury are anagrams."))
   end
-  # it('Will still check if inputs are anagrams if they have different cases') do
-  #   word = Anagrams.new()
-  #   expect(word.word_check('Eat Tea')).to(eq("Eat and Tea are anagrams"))
-  # end
+  it('Will still check if inputs are anagrams if they have different cases') do
+    word = Anagrams.new()
+    expect(word.word_check('Eat', 'Tea')).to(eq("Eat and Tea are anagrams."))
+  end
   # it('Will still check if inputs are antigrams') do
   #   word = Anagrams.new()
   #   expect(word.word_check('hi bye')).to(eq("hi and bye are antigrams"))
